bab533d3274d03f17867f48429ab14729352ea4f
wcheungucsd <wcheung@ucsd.edu>
2025-05-09 18:53:45 -0700

    Update start-notebook.sh

--- artifacts/start-notebook.sh
+++ artifacts/start-notebook.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-export PATH="/home/sage/sage/src/bin:/home/sage/sage/build/bin:/home/sage/sage/local/var/lib/sage/venv-python3.9.9/bin:/home/sage/sage/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${PATH}"
+#export PATH="/home/sage/sage/src/bin:/home/sage/sage/build/bin:/home/sage/sage/local/var/lib/sage/venv-python3.9.9/bin:/home/sage/sage/local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:${PATH}"
 
-exec sage -n jupyter --no-browser "$@"
+#exec sage -n jupyter --no-browser "$@"
+
+if [ x"$1" = x"sage-jupyter" ]; then
+    # If "sage-jupyter" is given as a first argument, we start a jupyter notebook
+    # with reasonable default parameters for running it inside a container.
+    shift
+    exec sage -n jupyter --no-browser --ip='0.0.0.0' --port=8888 "$@"
+else
+    exec sage -sh -c "$*"
+fi
